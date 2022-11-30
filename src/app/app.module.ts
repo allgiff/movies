5fef31a16378abc663f959bc505e4ec8aa1b3014
allgiff <gif19002@byui.edu>
2022-11-29 19:04:40 -0700

    created header and added bootstrap to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieListComponent } from './movies/movie-list/movie-list.component';
 import { MovieItemComponent } from './movies/movie-item/movie-item.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     MovieListComponent,
-    MovieItemComponent
+    MovieItemComponent,
+    HeaderComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/movies/movie.model.ts
@@ -0,0 +1,8 @@
+export class Movie {
+    constructor(
+        public id: string,
+        public name: string,
+        public description: string,
+        public imageUrl: string
+    ) {}
+}
