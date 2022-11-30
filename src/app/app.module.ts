5ffa215b0c0e2e36702f69ea26a911f259dd6a72
allgiff <gif19002@byui.edu>
2022-11-29 18:15:34 -0700

    started app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieItemComponent } from './movies/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieListComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule
